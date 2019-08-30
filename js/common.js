58e3b6487032b7703dffb687d534a2e191982fa2
artemsheludko <sheludko.artem@gmail.com>
2019-08-30 14:27:56 +0300

    fix rearch issue

--- js/common.js
+++ js/common.js
@@ -52,17 +52,6 @@ $(document).ready(function() {
   }
 
   /* =======================
-  // Simple Jekyll Search
-  ======================= */
-  SimpleJekyllSearch({
-    searchInput: document.getElementById("js-search-input"),
-    resultsContainer: document.getElementById("js-results-container"),
-    json: "/search.json",
-    searchResultTemplate: '<li class="search-results__item  wow fadeIn"><a class="search-results__link" href="{url}">{title}</a></li>',
-    noResultsText: '<li class="no-results">No results found</li>'
-  });
-
-  /* =======================
   // Responsive Videos
   ======================= */
   $(".post__content, .page__content").fitVids({
